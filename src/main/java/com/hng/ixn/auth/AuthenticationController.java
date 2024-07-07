0ca389257486590bb7c733bfcc7b8f4a55aebe38
nghugo <nghugoryan@gmail.com>
2024-07-07 20:25:42 +0100

    lombok

--- src/main/java/com/hng/ixn/auth/AuthenticationController.java
+++ src/main/java/com/hng/ixn/auth/AuthenticationController.java
@@ -1,28 +1,18 @@
 package com.hng.ixn.auth;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
 
-
     private final AuthenticationService service;
-
-    @Autowired
-    public AuthenticationController(AuthenticationService service){
-        this.service = service;
-    }
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
