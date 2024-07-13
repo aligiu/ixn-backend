58eecb103bc20e6cc88e22c286d57572934f37aa
nghugo <nghugoryan@gmail.com>
2024-07-13 22:59:15 +0100

    add admin@example.com registration on startup

---
+++ src/main/java/com/hng/ixn/DataInitializer.java
@@ -0,0 +1,31 @@
+package com.hng.ixn;
+
+import com.hng.ixn.user.Role;
+import com.hng.ixn.user.User;
+import com.hng.ixn.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.findByEmail("admin@example.com").isEmpty()) {
+            User admin = User.builder()
+                    .email("admin@example.com")
+                    .password(passwordEncoder.encode("SecurePassword123"))
+                    .role(Role.ROLE_ADMIN)
+                    .build();
+            userRepository.save(admin);
+        }
+    }
+}
